172b78d3b69b3f8088203812f290af00cf189f86
thomasvs <thomasvs@2b0047a9-a6d8-0310-accf-f7200b2a168c>
2005-12-20 19:05:14 +0000

    autogenerated
    git-svn-id: https://code.fluendo.com/flumotion/svn/cortado/trunk@2645 2b0047a9-a6d8-0310-accf-f7200b2a168c

--- com/fluendo/player/Configure.java
+++
@@ -1,9 +0,0 @@
-package com.fluendo.player;
-
-class Configure
-{
-  public String buildInfo = "I was built on Tue Dec 20 19:50:02 CET 2005 (version 0.1.2.1). Built using ant.";
-
-  public Configure() {
-  }
-}
